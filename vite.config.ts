ca2f32ec660ce9e2b354a9288469560c5653be1f
Sharath <sharath@saaslabs.co>
2023-02-08 15:42:45 +0530

    fix(config): Removed base

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import { config as DotEnv } from "dotenv";
 import type { UserConfig } from "vite";
-
-DotEnv();
 
 const config: UserConfig = {
   plugins: [sveltekit()],
-  base: process.env.BASE_URL ?? "/",
 };
 
 export default config;
